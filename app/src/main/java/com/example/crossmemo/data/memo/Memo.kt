1d9c57c398cda2ed20183cce56c0342ea4d19aba
Li Longzhen <koreabnl@gmail.com>
2025-08-21 18:20:10 +0900

    update library

--- app/src/main/java/com/example/crossmemo/data/memo/Memo.kt
+++ app/src/main/java/com/example/crossmemo/data/memo/Memo.kt
@@ -1,10 +1,12 @@
 package com.example.crossmemo.data.memo
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 data class Memo(
     val title: String,
     val content: String,
+    @PrimaryKey(autoGenerate = true)
     val id: Int
 )
